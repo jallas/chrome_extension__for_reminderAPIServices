72ed8d8979bbe08c05cb141e851d724aae769855
jallas <ajallau@yahoo.com>
2022-02-15 21:19:33 +0100

    update

--- src/api/firebase.js
+++
@@ -1,6 +0,0 @@
-const firebase = require("firebase-admin")
-firebase.initializeApp({ credential: firebase.credential.applicationDefault() });
-
-exports.FCM = ({ title, body }, token) => {
-  return firebase.messaging().send({ notification: { title, body }, token })
-}
